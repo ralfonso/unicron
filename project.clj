be6103cd15b1a19b65d8b828e8484ac8719b39ab
Ryan Roemmich <ralfonso@gmail.com>
2013-02-21 21:16:14 -0700

    bump ver

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject unicron "0.1.0"
+(defproject unicron "0.1.1"
   :description "Library for communicating with the Twitter Streaming API"
   :url "blah" ;FIXME: needs url
   :license {:name "Eclipse Public License" ;FIXME: needs license
