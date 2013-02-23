4a76b793815f91d5f6d3c8e4d91e1831417f3cf6
Ryan Roemmich <ralfonso@gmail.com>
2013-02-23 12:13:22 -0700

    tests need work!

--- test/unicron/core_test.clj
+++ test/unicron/core_test.clj
@@ -4,24 +4,14 @@
 
 (def backoff-intervals (-> default-config :backoff :intervals))
 
-(deftest test-backoff
-  (testing "http-backoff"
-    (is (= 5000 (dispatch-backoff {:http-error 2} :http-error backoff-intervals)))
-    (is (= 20000 (dispatch-backoff {:http-error 4} :http-error backoff-intervals))))
-  (testing "network-backoff"
-    (is (= 250 (dispatch-backoff {:network-error 2} :network-error backoff-intervals)))
-    (is (= 500 (dispatch-backoff {:network-error 3} :network-error backoff-intervals)))
-    (is (= 1500 (dispatch-backoff {:network-error 7} :network-error backoff-intervals)))))
-
 (deftest test-handle-error
   (testing "handle-error"
-    (let [base-control {:network-error 0 :http-error 0}
-          errors-in-control {:network-error 5 :http-error 1}]
-      (is (= (handle-error base-control :network-error) {:network-error 1 :http-error 0}))
-      (is (= (handle-error base-control :http-error) {:network-error 0 :http-error 1}))
-      (is (= (handle-error errors-in-control :disconnect) {:network-error 0 :http-error 0})))))
-
-(deftest test-twitter-stream-connect
-  (testing "network-error, connection refused"
-    (let [bad-url "http://NONEXISTENTHOSTNAME.nontld"]
-      (is (= :network-error (twitter-stream-connect (fn) bad-url {:user "none" :password "none"} { }))))))
+    (let [errors (atom {:intervals backoff-intervals :counts {:network-error 0 :http-error 0}})]
+      (handle-error errors :network-error)
+      (is (= (-> @errors :counts :network-error) 1)))
+    (let [errors (atom {:intervals backoff-intervals :counts {:network-error 0 :http-error 0}})]
+      (handle-error errors :http-error)
+      (is (= (-> @errors :counts :http-error) 1)))
+    (let [errors (atom {:intervals backoff-intervals :counts {:network-error 5 :http-error 3}})]
+      (handle-error errors :disconnect)
+      (is (= (-> @errors :counts) {:network-error 0 :http-error 0})))))
